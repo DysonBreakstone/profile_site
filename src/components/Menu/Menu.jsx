0d2cafff86dfacd53f68436b2b63754d94210aa0
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-10-16 15:53:33 -0600

    create side menu with placeholders

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default function Menu() {
+  return (
+    <nav className="flex flex-col bg-gray-700 bg-opacity-75 w-1/3 h-auto mr-6 py-8 rounded-lg gap-6 items-end">
+      <div className="flex w-2/3 border-white border-b-2 justify-end p-2 pr-6">
+        <h1 className="text-white">Menu</h1>
+      </div>
+      <div className="flex w-2/3 justify-end p-2 pr-6">
+        <h2 className="text-white">Item1</h2>
+      </div>
+      <div className="flex w-2/3 justify-end p-2 pr-6">
+        <h3 className="text-white">Item1</h3>
+      </div>
+      <div className="flex w-2/3 justify-end p-2 pr-6">
+        <h2 className="text-white">Item1</h2>
+      </div>
+      <div className="flex w-2/3 justify-end p-2 pr-6">
+        <h2 className="text-white">Item1</h2>
+      </div>
+      <div className="flex w-2/3 justify-end p-2 pr-6">
+        <h2 className="text-white">Item1</h2>
+      </div>
+    </nav>
+  )
+}
--- src/components/ProfilePanel/ProfilePanel.jsx
+++ src/components/ProfilePanel/ProfilePanel.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import profilePhoto from "./headshot5.jpg";
+import profilePhoto from "./headshot6.jpg";
+import Menu from "../Menu/Menu.jsx"
 
 export default function ProfilePanel() {
   return (
-    <div id="profilePanel" style={{ backgroundImage: `url(${profilePhoto})`}} className="w-full h-full rounded-lg bg-cover">
-      
+    <div id="profilePanel" style={{ backgroundImage: `url(${profilePhoto})`}} className="flex w-full h-full rounded-lg bg-cover items-center justify-end">
+      <Menu />
     </div>
   );
 };
