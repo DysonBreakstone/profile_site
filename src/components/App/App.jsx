75fa11f20ed475e3954f3471b6e1f7a505c43956
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-10-16 15:52:24 -0600

    main div styling, useState for background

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import ProfilePanel from "../ProfilePanel/ProfilePanel.jsx"
+import { BACKGROUND_COLORS } from "../../constants/colors.js";
 import './App.css'
 
 function App() {
-  const [mainBgColor, setMainBgColor] = useState("white");
+  const [mainBgColor, setMainBgColor] = useState(BACKGROUND_COLORS.sixth);
 
   function changeBackgroundColor(color) {
     console.log("yo!")
   }
 
+  function changeBackground(color) {
+    this.setMainBgColor(color)
+  }
+
   return (
-    <main className="w-full h-full rounded-lg">
+    <main className="w-screen h-screen p-8" style={{background: `${mainBgColor}`}}>
       <ProfilePanel />
     </main>
   )
