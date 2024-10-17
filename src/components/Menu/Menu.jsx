4b47fe512d11e9aecc0396a279dc4c0c995cab61
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-10-17 16:20:10 -0600

    add ProfileMenu component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,27 +1,14 @@
 import React from "react";
+import ProfileMenu from "./ProfileMenu";
 
 
-export default function Menu() {
+export default function Menu({ changeBackground }) {
   return (
     <nav className="flex flex-col bg-gray-700 bg-opacity-75 w-1/3 h-auto mr-6 py-8 rounded-lg gap-6 items-end">
       <div className="flex w-2/3 border-white border-b-2 justify-end p-2 pr-6">
-        <h1 className="text-white">Menu</h1>
+        <h2 className="text-white">Dyson Breakstone</h2>
       </div>
-      <div className="flex w-2/3 justify-end p-2 pr-6">
-        <h2 className="text-white">Item1</h2>
-      </div>
-      <div className="flex w-2/3 justify-end p-2 pr-6">
-        <h3 className="text-white">Item1</h3>
-      </div>
-      <div className="flex w-2/3 justify-end p-2 pr-6">
-        <h2 className="text-white">Item1</h2>
-      </div>
-      <div className="flex w-2/3 justify-end p-2 pr-6">
-        <h2 className="text-white">Item1</h2>
-      </div>
-      <div className="flex w-2/3 justify-end p-2 pr-6">
-        <h2 className="text-white">Item1</h2>
-      </div>
+      <ProfileMenu changeBackground = {changeBackground} />
     </nav>
   )
 }
