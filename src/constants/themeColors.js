a813498e60b6872b28d963905066e618ef4834bb
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-10-18 09:40:08 -0600

    change name of color constant

--- src/constants/themeColors.js
+++ src/constants/themeColors.js
@@ -1,7 +1,7 @@
 export const THEME_COLORS = {
   "other": "white",
-  "About": '#D6F0E2',
-  "Peg Game": '#FFF6DB',
+  "Peg Game": '#D6F0E2',
+  "About": '#FFF6DB',
   "Projects": '#BFD8C8',
   "Pottery": '#A3BCCB',
   "Experience": '#E8EBCF',
