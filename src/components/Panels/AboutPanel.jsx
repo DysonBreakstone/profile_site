f6904965676e33876f86d6676c3cc5fde89900d5
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-10-23 09:32:04 -0600

    update description

--- src/components/Panels/AboutPanel.jsx
+++ src/components/Panels/AboutPanel.jsx
@@ -23,14 +23,16 @@ export default function AboutPanel() {
           <h3 id="snapshot" className="text-slate-600 pb-6">Snapshot</h3>
           <p id="aboutDescription" className="text-slate-700 indent-4 text-justify">
             I am an early-career software engineer with 1.5 years of experience in web development.
-            My experience is primarily back-end focused, working with data-heavy web applications, and my first love
-            will always be building and working with relational databases. However, I have professional experience 
-            in almost every aspect of web development from UI/UX to designing and consuming APIs, to customer satisfaction.
+            My experience is primarily back-end focused, working with data-heavy web applications, 
+            and my first love will always be developing databases and APIs. However, during my time 
+            working in a startup environment, I have gained experience in nearly every facet of web 
+            development, including UI/UX design, database management, and direct collaboration with 
+            clients.
           </p>
           <p id="aboutPersonal" className="text-slate-700 indent-4 text-justify">
-            I live in Boulder, Colorado with my partner Helen. When I'm not designing and implementing 
-            world-class relational databases or spending time at the pottery studio, she and I are exploring
-            Colorado's great outdoors or traveling the world together. 
+            I was born in Vermont, and now reside in Boulder, Colorado with my partner Helen. When 
+            I'm not coding, I enjoy rock climbing, overly complicated boardgames, and traveling 
+            abroad. 
           </p>
         </section>
         <section id="aboutBuffer" className="flex min-w-96"></section>
