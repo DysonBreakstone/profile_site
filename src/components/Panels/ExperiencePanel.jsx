ea87bcfb6db4ac354922dd839f3a089ceac1abb2
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-10-24 11:19:30 -0600

    add image assets for work experience

--- src/components/Panels/ExperiencePanel.jsx
+++ src/components/Panels/ExperiencePanel.jsx
@@ -3,7 +3,8 @@ import { THEME_COLORS } from "../../constants/themeColors";
 import jobExperience from "../../constants/jobExperience";
 import "./Panel.css"
 
-export default function ExperiencePanel({ changeBackground }) {
+
+export default function ExperiencePanel() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
@@ -12,18 +13,18 @@ export default function ExperiencePanel({ changeBackground }) {
 
   const jobs = jobExperience.map(job =>
     <li
-      key={`${job["organization"]}Card`}>
-        <div className="experience-card">
+      key={`${job["organization"]}Card`} className="flex">
+        <div className="experience-card m-4 w-2/3">
           <div className="flex w-full justify-between">
-            <h3 className="pb-1" >{job["organization"]}</h3>
+            <h3 className="pb-1" >{job.organization}</h3>
             <a href={job["website"]} className="mb-1">Visit the website</a>
           </div>
           <div className="pb-2 border-b-2 w-full text-end mb-6">
-            <h5 className="text-slate-700 pb-1">{job["title"]}</h5>
-            <h5 className="text-slate-700 pb-1">{job["dates"]}</h5>
+            <h5 className="text-slate-700 pb-1">{job.title}</h5>
+            <h5 className="text-slate-700 pb-1">{job.dates}</h5>
           </div>
           <p className="font-bold mb-2">Description</p>
-          <p className="indent-4 mb-2">{job["description"]}</p>
+          <p className="indent-4 mb-2">{job.description}</p>
           {job["accomplishments"] &&
             <div>
               <p className="font-bold mb-2">My contributions</p>
@@ -37,15 +38,18 @@ export default function ExperiencePanel({ changeBackground }) {
             </div>
           }
         </div>
+        <div className="flex w-1/3 items-center justify-center">
+          <img src={job.logoUrl} className="aspect-auto h-1/2 rounded-xl"></img>
+        </div>
     </li>
     )
 
   return (
     <div 
       id="profilePanel"  
-      className={`h-full pt-36 overflow-auto flex w-full bg-cover items-center justify-left transition-opacity duration-300 p-8 ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
+      className={`h-full pt-32 overflow-auto flex w-full bg-cover justify-left transition-opacity duration-300 p-8 ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
       style={{ backgroundColor: THEME_COLORS["panelDiv"] }}>
-        <ul className="flex flex-col gap-6 space-y-4 w-2/3">
+        <ul className="flex flex-col">
           {jobs}
         </ul>
     </div>
