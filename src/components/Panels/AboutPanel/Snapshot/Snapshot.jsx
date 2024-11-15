1df0a3ccc34240fd78130df8c8bcabd826df767b
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-11-15 11:56:40 -0700

    move socials

--- src/components/Panels/AboutPanel/Snapshot/Snapshot.jsx
+++ src/components/Panels/AboutPanel/Snapshot/Snapshot.jsx
@@ -30,12 +30,23 @@ export default function Snapshot() {
       <section className="flex w-full h-full bg-dark bg-opacity-30 p-8">
         <section id="aboutInfo" className="w-full text-white flex flex-col items-start text-start justify-between h-full">
           <div className="flex flex-col md:flex-row w-full md:justify-between md:mt-20 h-full">
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-between md:justify-start gap-12 md:gap-28">
               <div className="flex flex-col gap-2 md:gap-8">
                 <h1>Dyson Breakstone</h1>
                 <h2 className="text-hero-text-accent">Software Engineer</h2>
                 <h2 className="text-hero-text-accent">Data Enthusiast</h2>
                 <h2 className="text-hero-text-accent">Lifelong Learner</h2>
+              </div>
+              <div className="flex flex-col gap-8">
+                <h2 className="flex text-hero-text-accent-2">Socials</h2>
+                <div className="flex gap-8">
+                  <a href="https://www.linkedin.com/in/dysonbreakstone/">
+                    <img src={linkedInLogo} className="w-12 h-12"></img>
+                  </a>
+                  <a href="https://github.com/DysonBreakstone">
+                    <img src={githubLogo} className="w-12 h-12"></img>
+                  </a>
+                </div>
               </div>
             </div>
             <div className="flex flex-col gap-12 md:gap-28 mt-10 md:mt-0">
@@ -50,17 +61,6 @@ export default function Snapshot() {
                   <p style={{ fontSize: '1rem' }}>{import.meta.env.VITE_PHONE_NUMBER || process.env.VITE_PHONE_NUMBER}</p>
                 </div>
                 <button onClick={onButtonClick} className="bg-secondary-accent p-2 rounded-full text-white w-max hover:text-dark">Download Resume</button>
-              </div>
-              <div className="flex flex-col gap-8">
-                <h2 className="flex text-hero-text-accent-2">Socials</h2>
-                <div className="flex gap-8">
-                  <a href="https://www.linkedin.com/in/dysonbreakstone/">
-                    <img src={linkedInLogo} className="w-12 h-12"></img>
-                  </a>
-                  <a href="https://github.com/DysonBreakstone">
-                    <img src={githubLogo} className="w-12 h-12"></img>
-                  </a>
-                </div>
               </div>
             </div>
           </div>
